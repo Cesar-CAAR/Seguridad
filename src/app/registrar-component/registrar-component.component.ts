da1b4c7f3114d5b221f29ec92b996f60eb451b69
Harol Cisneros <harolcsnrs@gmail.com>
2025-12-02 17:37:28 -0700

    registro fucional

--- src/app/registrar-component/registrar-component.component.ts
+++ src/app/registrar-component/registrar-component.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-registrar-component',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './registrar-component.component.html',
   styleUrl: './registrar-component.component.css'
 })
-
 export class RegistrarComponentComponent {
 
+  usuario = '';
+  password = '';
+  confirmPassword = '';
+
+  constructor(private api: ApiService) {}
+
+  registrar() {
+
+    if (this.password !== this.confirmPassword) {
+      alert("Las contraseñas no coinciden");
+      return;
+    }
+
+    this.api.register(this.usuario, this.password).subscribe({
+      next: (resp) => {
+        alert("Usuario registrado correctamente");
+      },
+      error: (err) => {
+        alert("Error al registrar usuario");
+      }
+    });
+
+  }
 }
